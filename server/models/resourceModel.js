6e703ea9b3e3675bac99fd46eae26f4e18b4ca09
jenpoole <contact.vandepool@gmail.com>
2021-04-08 17:43:25 -0400

    setup Google Auth credentials

--- server/models/resourceModel.js
+++ server/models/resourceModel.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const resourceSchema = new Schema({
     link: { type: String, required: true },
-    teamId: { type: String, required: true },
+    teamId: { type: String, required: true }, // objectID
     votes: { type: Number, default: 0 },
-    category: { type: String, required: true }
+    category: { type: String, required: true } // change to tags
 }, { timestamps: true });
 
 var Resource = mongoose.model('Resource', resourceSchema);
